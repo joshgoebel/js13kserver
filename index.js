ce386c76650bb3242b4fb86cbb2773e19252d403
Josh Goebel <me@joshgoebel.com>
2020-08-17 09:01:37 -0400

    show

--- index.js
+++ index.js
@@ -13,12 +13,22 @@ const shared = fs.existsSync('./public/shared.js') ? fs.readFileSync('./public/s
 const storage = require('./lib/storage');
 const limit = 13 * 1024;
 const chalk = require('chalk');
+const { minify } = require("terser");
+const gulp = require('gulp');
 const runMode = process.env.NODE_ENV || 'development'
 const isDevelopment = !!(runMode === 'development')
 const isProduction = !!(runMode === 'production')
 const isTest = !!(runMode === 'test')
 
+const { execSync } = require("child_process");
+
+// gulp stuff
+var gulpif =    require('gulp-if');
+var terser =    require('gulp-terser')
+
+
 let packageSize = 0;
+let production = true
 
 function createSandbox() {
     const sandbox = {
@@ -43,12 +53,59 @@ function createSandbox() {
     return sandbox;
 };
 
-function createZip() {
-    return new Promise((resolve, reject) => {
+const build_shared =  () => {
+    return gulp.src("public/shared.js")
+    //   .pipe(rollup({
+    //   }, {
+    //     format: "iife",
+    //     name: "IIFE"
+    //   }))
+    //   .pipe(concat('build.js'))
+      .pipe(gulpif(production, terser()))
+      .pipe(gulp.dest('build'));
+  };
+
+const build_client =  () => {
+    return gulp.src("public/client.js")
+//   .pipe(rollup({
+//   }, {
+//     format: "iife",
+//     name: "IIFE"
+//   }))
+//   .pipe(concat('build.js'))
+    .pipe(gulpif(production, terser()))
+    .pipe(gulp.dest('build'));
+};
+
+const build_server =  () => {
+    return gulp.src("public/server.js")
+    //   .pipe(rollup({
+    //   }, {
+    //     format: "iife",
+    //     name: "IIFE"
+    //   }))
+    //   .pipe(concat('build.js'))
+        .pipe(gulpif(production, terser()))
+        .pipe(gulp.dest('build'));
+    };
+
+
+ async function createZip() {
+    execSync("rm -f build/*")
+
+    packageSize = -1;
+
+    await build_shared();
+    await build_client();
+    await build_server();
+
+    console.log(execSync("ls -l build").toString())
+    return  new Promise((resolve, reject) => {
         const archive = archiver('zip', {zlib: { level: 9 }});
         const output = fs.createWriteStream('dist.zip');
         output.on('close', () => {
             packageSize = archive.pointer();
+            console.log(execSync("ls -l dist.zip").toString())
             resolve(packageSize);
         });
         output.on('error', (error) => {
@@ -60,7 +117,7 @@ function createZip() {
             reject(error);
         })
         archive.pipe(output);
-        archive.directory('public/', '');
+        archive.directory('build/', '');
         archive.finalize();
     })
 };
@@ -108,20 +165,20 @@ storage.init(app.get('storage')).then(() => {
             }
         }
     }
-    server.listen(app.get('port'), () => {
+    server.listen(app.get('port'), async () => {
         console.log(chalk.blue(`Server started in ${runMode} mode at port ${app.get('port')}`));
-        createZip()
-        .then(()=> {
+        await createZip()
+        // .then(()=> {
             if (isDevelopment) {
                 let pct = packageSize / limit * 100;
                 let color = pct <= 100 ? chalk.green : chalk.red;
                 console.log(color(`Package: ${packageSize} byte / ${pct.toFixed(2)}%`));
                 recurrentLogStorageSize();
             }
-        })
-        .catch((err)=> {
-            console.error(err.message)
-        });
+        // })
+        // .catch((err)=> {
+        //     console.error(err.message)
+        // });
     });
 }).catch(err => {
     console.error(err);
